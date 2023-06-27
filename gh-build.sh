721a8edb3bc27a6cf055c2a690c339be609294ac
Uri Herrera <hello@uriherrera.com>
2023-06-27 14:50:32 -0600

    add build stuff

---
+++ gh-build.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+set -x
+
+### Clone the source
+git clone --depth=1 https://gitlab.freedesktop.org/hadess/power-profiles-daemon.git src
+mv src/* . && rm -r src
+
+### Basic Packages
+apt -qq -yy install equivs git devscripts lintian --no-install-recommends
+
+### Install Dependencies
+mk-build-deps -i -t "apt-get --yes" -r
+
+### Build Deb
+debuild -b -uc -us
+
+### Move Deb to current directory because debuild decided
+### that it was a GREAT IDEA TO PUT THE FILE ONE LEVEL ABOVE
+mv ../*.deb .
